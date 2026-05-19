93ebbf5db9b5445362397ff821911da0b94ee58f
WeihaoWu30 <weihaowu738@gmail.com>
2026-05-19 12:40:26 -0400

    initial setup

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import styles from "./page.module.css";
+
+export default function Dashboard() {
+  return (
+    <div className={styles.page}>
+    </div>
+  );
+}
---
+++ frontend/app/groceries/page.tsx
@@ -0,0 +1,8 @@
+import styles from "./page.module.css";
+
+export default function Groceries() {
+  return (
+    <div className={styles.page}>
+    </div>
+  );
+}
