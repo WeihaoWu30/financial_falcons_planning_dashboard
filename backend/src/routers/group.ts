8d93a6df5d10794bb84025d404be5834dfa70509
WeihaoWu30 <weihaowu738@gmail.com>
2026-05-20 13:48:03 -0400

    routers work

--- backend/src/routers/group.ts
+++ backend/src/routers/group.ts
@@ -3,6 +3,15 @@ import { router, publicProcedure, protectedProcedure } from "../../trpc.js"
 import { prisma } from "../../prisma.js"
 
 export const groupRouter = router({
+
+   getByUser: protectedProcedure
+      .input(z.object({userID: z.string()}))
+      .query(async({input}) =>{
+         return await prisma.group.findUnique({
+            where: {userID: input.userID}
+         })
+      }),
+
    create: publicProcedure
       .input(z.object({name: z.string(), userID: z.string()}))
       .mutation(async ({input }) =>{
@@ -10,11 +19,32 @@ export const groupRouter = router({
             data: {name: input.name, userID: input.userID},
          });
       }),
+
+
    getByID: protectedProcedure
       .input(z.object({id: z.number()}))
       .query(async({input})=>{
          return await prisma.group.findUnique({
             where: {id: input.id}
          })
+      }),
+
+   addMember: protectedProcedure
+      .input(z.object({name: z.string(), groupID: z.number()}))
+      .mutation(async ({input}) =>{
+         return await prisma.member.create({
+            data: {name: input.name, groupID: input.groupID},
+         });
+      }),
+
+   getMembers: protectedProcedure
+      .input(z.object({id: z.number()}))
+      .query(async({input})=>{
+         return await prisma.group.findUnique({
+            where: {id: input.id},
+            select:{
+               members: true
+            }
+         });
       })
-});
+})
